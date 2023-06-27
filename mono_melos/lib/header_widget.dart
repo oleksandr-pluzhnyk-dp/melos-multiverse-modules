06ef519b0167558613d0e299ab55a3ea49d6f968
Murnyk, Mykhailo <mykhailo.murnyk@xenial.com>
2023-06-27 13:51:23 -0400

    create melos

---
+++ mono_melos/lib/header_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HeaderWidget extends StatefulWidget {
+  const HeaderWidget({Key? key}) : super(key: key);
+  @override
+  State<HeaderWidget> createState() => _HeaderWidgetState();
+}
+
+class _HeaderWidgetState extends State<HeaderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+      width: 200,
+      height: 200,
+    );
+  }
+}
--- mono_melos/lib/main.dart
+++ mono_melos/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mono_melos/header_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -105,6 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const HeaderWidget(),
             const Text(
               'You have pushed the button this many times:',
             ),
