20d9cebcd9d3b2ebde428a89b5fd8528de4e2ad8
Murnyk, Mykhailo <glamurnik90@gmail.com>
2023-06-27 15:40:49 -0400

    feat: add black color
    feat: add black color

--- mono_melos/packages/header_module/lib/header_widget.dart
+++ mono_melos/packages/header_module/lib/header_widget.dart
@@ -10,7 +10,7 @@ class _HeaderWidgetState extends State<HeaderWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      color: Colors.black,
       width: 200,
       height: 200,
     );
