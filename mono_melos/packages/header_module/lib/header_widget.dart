0159981528e63973dbb27d63f79763907d026d20
Murnyk, Mykhailo <glamurnik90@gmail.com>
2023-06-27 15:06:25 -0400

    package change color and add tag

--- mono_melos/packages/header_module/lib/header_widget.dart
+++ mono_melos/packages/header_module/lib/header_widget.dart
@@ -10,7 +10,7 @@ class _HeaderWidgetState extends State<HeaderWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.yellow,
+      color: Colors.blue,
       width: 200,
       height: 200,
     );
